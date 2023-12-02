db8e34aceb747118ae7866c64fea525942920ae9
Felipe González Poblete <gonzalezpobletefelipe@gmail.com>
2023-12-02 08:49:23 -0300

    subiendo los archivos que estaban en el pc del crecer

--- Optimizador de agenda.py
+++ Optimizador de agenda.py
@@ -1,4 +1,9 @@
 import numpy as np
 import pandas as pd
-import datetime
+import datetime as dt
+
+#lo que se pretende con este programa es recibir un documento en formato .csv,
+#en el cual se pondran los profesionales junto a sus dias y horarios de atencion
+#para luego retornar un archivo en formato .csv, en el que entrega las salas minimas 
+#para atencion
 
