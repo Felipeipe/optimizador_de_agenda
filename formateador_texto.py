0c8baaf0dd525c9ed60015b81b43305e71499e3f
Felipe González Poblete <gonzalezpobletefelipe@gmail.com>
2023-12-05 20:47:25 -0300

    agregado una parte del formateador de archivo

---
+++ formateador_texto.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from datetime import time
+
+df = pd.DataFrame(pd.read_excel("listado de profesionales.xlsx"))
+
+i=1
+I=[]
+D={}
+while i<len(df.index):
+    df[i,:]
+    i+=2
+
+
+
